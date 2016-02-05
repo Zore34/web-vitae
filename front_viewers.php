1fe5d68b087add09959c321bfa2e9f26fcfe1387
Javier Paz Sedano <zore34@gmail.com>
2016-02-05 14:56:27 +0100

    Update front_viewers.php
    Outlined the functions in the module

--- front_viewers.php
+++ front_viewers.php
@@ -1,3 +1,41 @@
 <?php
 /* In this file we define viewers for the front page (page visible to anybody visiting the website).
  */
+
+/**
+ * This class is used for definin a namespace for the functions related to front viewers
+ */
+class FrontViewers{
+  /**
+   * This function returns a HTML code with the view of the section we want to draw in the main area.
+   * @param section The identifier of the section we want to draw.
+   * @returns string View of the generated section
+   */
+  static function getSectionViewer($section){
+    
+  }
+  
+  /**
+   * Returns HTML representing the navigation bar.
+   * TODO: If the nav bar is static enough calling it could be simply including the HTML file.
+   */
+  static function navBarViewer() {
+    
+  }
+  
+  /**
+   * Returns HTML representing the top bar of the site.
+   * TODO: If the top bar is static enough calling it could be simply including the HTML file.
+   */
+  static function topBarViewer() {
+    
+  }
+  
+  /**
+   * Returns HTML representing the bottom bar fo the site.
+   * TODO: If the bottom bar is static enough calling it could be simply including the HTML file.
+   */
+  static function bottomViewer() {
+    
+  }
+}
