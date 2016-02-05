042680132887abbd63ff4e034682dd957facab9f
Javier Paz Sedano <zore34@gmail.com>
2016-02-05 15:01:53 +0100

    Update config_params.php
    Initial outline of the class

--- config_params.php
+++ config_params.php
@@ -1,3 +1,19 @@
 <?php
 /* In this file we define functions for reading and writing config parameters from storage.
  */
+
+/**
+ * Class used to define a namespace for the functions. The config files are in *.php format for performance.
+ */
+class ConfigParams {
+  private static $CONFIG_FOLDER = "./config";
+  
+  /**
+   * This function returns the styles configured in the site.
+   */
+  static function getStyles() {
+    
+  }
+  
+  
+}
