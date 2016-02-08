a54dd2d8261d9e168eef1e286eccf24709c56a03
Javier Paz Sedano <zore34@gmail.com>
2016-02-08 15:09:47 +0100

    Update top_viewer.php

--- viewers/top_viewer.php
+++ viewers/top_viewer.php
@@ -1,2 +1,23 @@
-<html>
-</html>
+<?php
+/* This module contains the HTML for the header of the page.
+ */
+require_once "../config_params.php"
+?>
+<header>
+  <div id="title">
+    
+  </div>
+  <?php
+  if(validLoginCode()){
+  ?>
+    <div>
+      
+    </div>
+  <?php
+  }else{
+  ?>
+    
+  <?php
+  }
+  ?>
+</header>
