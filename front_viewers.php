f75e73d5eacc481fe91bedb55006eae9abe653e2
Javier Paz Sedano <zore34@gmail.com>
2016-02-09 12:33:03 +0100

    Update front_viewers.php
    Added some functions and completed empty ones.

--- front_viewers.php
+++ front_viewers.php
@@ -2,40 +2,62 @@
 /* In this file we define viewers for the front page (page visible to anybody visiting the website).
  */
 
+require_once "config_params.php";
+
 /**
  * This class is used for definin a namespace for the functions related to front viewers
  */
 class FrontViewers{
   /**
-   * This function returns a HTML code with the view of the section we want to draw in the main area.
+   * This function prints a HTML code with the view of the section we want to draw in the main area.
    * @param section The identifier of the section we want to draw.
-   * @returns string View of the generated section
    */
   static function getSectionViewer($section){
-    
+    // TODO: Isn't necessary any aditional checks?
+    include "viewers/section_$section.php";
   }
   
   /**
-   * Returns HTML representing the navigation bar.
-   * TODO: If the nav bar is static enough calling it could be simply including the HTML file.
+   * This function retuns the CSS styles asociated with the front page and it's config.
+   * @return An array of the configured CSS styles.
    */
-  static function navBarViewer() {
-    
+  static function getCSSStyles() {
+    return ConfigParams::getFrontCSS();
   }
   
   /**
-   * Returns HTML representing the top bar of the site.
+   * Prints HTML representing the navigation bar.
+   */
+  static function getNavBarViewer() {
+    include "viewers/navBarViewer.php";
+  }
+  
+  /**
+   * Prints HTML representing the top bar of the site.
    * TODO: If the top bar is static enough calling it could be simply including the HTML file.
    */
-  static function topBarViewer() {
-    
+  static function getHeaderViewer() {
+    include "viewers/top_viewer.php";
   }
   
   /**
-   * Returns HTML representing the bottom bar fo the site.
-   * TODO: If the bottom bar is static enough calling it could be simply including the HTML file.
+   * Prints HTML representing the bottom bar fo the site.
    */
-  static function bottomViewer() {
-    
+  static function getBottomViewer() {
+    include "viewers/bottom_viewer.php";
+  }
+  
+  /**
+   * Prints HTML representing the login form. It only makes sense if the user isn't logged in.
+   */
+  static function getLoginForm() {
+    include "viewers/loginForm.php";
+  }
+  
+  /**
+   * Prints HTML representing the name of the user and a link to logout. It only makes sense if the user is logged in.
+   */
+  static function getLoginIdentifier() {
+    include "viewers/loginIdentifier.php";
   }
 }
