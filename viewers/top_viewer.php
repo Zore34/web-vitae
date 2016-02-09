32ac1a82370f11e37f365a135ece981defd9e7eb
Javier Paz Sedano <zore34@gmail.com>
2016-02-09 12:09:06 +0100

    Update top_viewer.php
    Basic structure defined.

--- viewers/top_viewer.php
+++ viewers/top_viewer.php
@@ -1,23 +1,22 @@
 <?php
 /* This module contains the HTML for the header of the page.
  */
-require_once "../config_params.php"
+require_once "../config_params.php";
+require_once "../front_viewers.php";
 ?>
 <header>
   <div id="title">
-    
+    <?php echo ConfigParams::getTitle();?>
   </div>
-  <?php
-  if(validLoginCode()){
-  ?>
-    <div>
-      
-    </div>
-  <?php
-  }else{
-  ?>
-    
-  <?php
-  }
-  ?>
+  <div id="login-area">
+    <?php
+    if(validLoginCode()){
+      // User is logged in, so we show the login identifier
+      echo FrontViewers::getLoginIdentifier();
+    }else{
+      // User isn't logged in, so we show the login form.
+      echo FrontViewers::getLoginForm();
+    }
+    ?>
+  </div>
 </header>
