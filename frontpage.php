2d354e6e6f5997484cb0647776e1fce674465c71
Javier Paz Sedano <zore34@gmail.com>
2016-02-12 08:36:49 +0100

    Update frontpage.php
    Added section viewers calls and JS links

--- frontpage.php
+++ frontpage.php
@@ -5,18 +5,29 @@
     require_once "config_params.php";
     require_once "default_params.php"
     // This file shows the frontpage of the site. Depending of the params, shows one section or another.
-    // TODO: Function getSectionViewer is not defined yet. Should be in front_viewers.php
-    $viewer = getSectionViewer(defaultParam('section'));
     // We define the styles
-    foreach(getStyles(defaultParam('style')) as $style){
+    foreach(FrontViewers::getStyles(defaultParam('style')) as $style){
     ?>
-    <link rel="stylesheet" type="text/css" href="<?php print $style ?>">
+    <link rel="stylesheet" type="text/css" href="<?php print $style;?>">
     <?php
+    }
+    // And define the active JavaScripts
+    foreach(FrontViewers::getJS() as $js){
+      ?>
+      <script type="text/javascript" src="<?php print $js;?>"></script>
+      <?php
     }
     ?>
     <title><?php print getTitle() ?></title>
   </head>
   <body>
-    
+    <?php
+    // Print the front page parts: header, nav bar, active section body and footer
+    // TODO: We could call FrontViewers or BackViewers depending on a param.
+    FrontViewers::getHeaderViewer();
+    FrontViewers::getNavBarViewer();
+    FrontViewers::getSectionViewer(defaultParam('section'));
+    FrontViewers::getFooterViewer();
+    ?>
   </body>
 </html>
