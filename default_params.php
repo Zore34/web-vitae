14de1d83610d0d6d4a9d9cc1f2484ba7efe0bc1a
Javier Paz Sedano <zore34@gmail.com>
2016-02-12 09:10:51 +0100

    Update default_params.php
    Added defaultParam function and defined the defaults array.

--- default_params.php
+++ default_params.php
@@ -1,3 +1,18 @@
 <?php
 /* In this file we define default values for every valid param to have a value if we don't get one from URL.
  */
+ 
+// Defining default values.
+var $defaults = Array(
+  "section" => "blog",
+  "style" => []
+  );
+ 
+/**
+ * Returns the default value for a param.
+ * @param $param The parameter 
+ * @return 
+ */
+function defaultParam($param){
+  return $defaults[$param];
+}
